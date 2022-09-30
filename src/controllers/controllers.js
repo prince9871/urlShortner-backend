03f3b3f6f7a8e63a50c7284107ab8d55817bd9e0
FirozSkAhmad <you@example.com>
2022-09-30 11:22:26 +0530

    extraValidations

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -7,14 +7,19 @@ async function url(req, res) {
   try {
     const data = req.body;
 
-    if(Object.keys(data).length===0){
-        return res
-        .status(400)
-        .send({ status: false, message: "required data" });
+    if (Object.keys(data).length === 0) {
+      return res.status(400).send({ status: false, message: "required data" });
     }
 
-    if(!isValidString(data.longUrl)){
-        return res
+    if (!Object.keys(data).includes("longUrl")) {
+      return res.status(400).send({
+        status: false,
+        message: "required longUrl key in request body",
+      });
+    }
+
+    if (!isValidString(data.longUrl)) {
+      return res
         .status(400)
         .send({ status: false, message: "longUrl must be in string format" });
     }
@@ -25,7 +30,7 @@ async function url(req, res) {
         .send({ status: false, message: "invalid longUrl" });
     }
 
-    const urlDocument = await urlModel.findOne(data);
+    const urlDocument = await urlModel.findOne({ longUrl: data.longUrl });
     if (urlDocument) {
       return res.status(200).send({ status: true, data: urlDocument });
     }
@@ -41,21 +46,25 @@ async function url(req, res) {
   }
 }
 
-
-const getUrl = async function(req, res){
+const getUrl = async function (req, res) {
   try {
-    let code = req.params
-    if(code.urlCode===":urlCode"){
-      return res.status(400).send({status: false, msg: "require urlCode"})
+    let code = req.params;
+    if (code.urlCode === ":urlCode") {
+      return res.status(400).send({ status: false, msg: "required urlCode" });
     }
-    const checkUrl = await urlModel.findOne({urlCode: code.urlCode})
-    if(!checkUrl){
-      return res.status(404).send({status: false, msg: "urlCode not found"})
+    if(!shortId.isValid(code.urlCode)){
+      return res.status(400).send({ status: false, msg: "invalid urlCode" });
     }
-    let longCode = checkUrl.longUrl
-    return res.status(302).send(`Found. Redirecting to: ${longCode}`)
+    const checkUrl = await urlModel.findOne({ urlCode: code.urlCode });
+    if (!checkUrl) {
+      return res
+        .status(404)
+        .send({ status: false, msg: "urlCode not founded" });
+    }
+    let longCode = checkUrl.longUrl;
+    return res.status(302).redirect(longCode);
   } catch (error) {
-    return res.status(500).send({status: false, msg: error.message})
+    return res.status(500).send({ status: false, msg: error.message });
   }
-}
+};
 module.exports = { url, getUrl };
