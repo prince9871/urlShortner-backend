ac30d3e93093e16e9171382b1677faead37c35e5
FirozSkAhmad <you@example.com>
2022-10-03 17:27:05 +0530

    caching

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -3,6 +3,32 @@ const urlModel = require("../models/urlModel");
 const validUrl = require("valid-url");
 const { isValidString } = require("../validations/validations");
 
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  13489,
+  "redis-13489.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("53kQRUXp3EYwFNgxmttXuVawmIr2ifmx", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 async function url(req, res) {
   try {
     const data = req.body;
@@ -30,23 +56,37 @@ async function url(req, res) {
         .send({ status: false, message: "invalid longUrl" });
     }
 
-    const urlDocument = await urlModel
-      .findOne({ longUrl: data.longUrl })
-      .select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 });
+    // const urlDocument = await urlModel
+    //   .findOne({ longUrl: data.longUrl })
+    //   .select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 });
+    // if (urlDocument) {
+    //   return res.status(200).send({ status: true, data: urlDocument });
+    // }
+
+    let urlDocument = await GET_ASYNC(`${req.body.longUrl}`);
     if (urlDocument) {
-      return res.status(200).send({ status: true, data: urlDocument });
+      return res.status(200).send(urlDocument);
     }
+    // else {
+    //   let urlD = await urlModel
+    //     .findOne({ longUrl: data.longUrl })
+    //     .select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 });
+    //   if (urlD) {
+    //     await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(urlD));
+    //     return res.status(700).send({ data: urlD });
+    //   }
+    // }
 
     data.urlCode = shortId.generate(data.longUrl);
     data.shortUrl = `http://localhost:3000/${data.urlCode}`;
 
     const urlDoc = await urlModel.create(data);
-    const doc=urlDoc.toObject()
-    delete doc["_id"]
-    delete doc["createdAt"]
-    delete doc["updatedAt"]
-    delete doc["__v"]
-
+    const doc = urlDoc.toObject();
+    delete doc["_id"];
+    delete doc["createdAt"];
+    delete doc["updatedAt"];
+    delete doc["__v"];
+    await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(doc));
     return res.status(201).send({ status: true, data: doc });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
@@ -62,13 +102,23 @@ const getUrl = async function (req, res) {
     if (!shortId.isValid(code.urlCode)) {
       return res.status(400).send({ status: false, msg: "invalid urlCode" });
     }
+
+    let ckUrl = await GET_ASYNC(`${req.params.urlCode}`);
+    if (ckUrl) {
+      return res.status(302).redirect(ckUrl);
+    }
+
     const checkUrl = await urlModel.findOne({ urlCode: code.urlCode });
     if (!checkUrl) {
       return res
         .status(404)
         .send({ status: false, msg: "urlCode not founded" });
     }
+
     let longCode = checkUrl.longUrl;
+
+    await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(longCode));
+    
     return res.status(302).redirect(longCode);
   } catch (error) {
     return res.status(500).send({ status: false, msg: error.message });
